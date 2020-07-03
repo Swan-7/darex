a9727233e483bcdda5253d5b78f6220d347c3fbc
Philemon Gumah <48827922+phil196000@users.noreply.github.com>
2020-07-03 19:15:34 +0000

    working updates
    commit before payment verify api with node

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,162 @@
 import React from "react";
 import Navbar from "../NavBar/Navbar";
+import "./Header.css";
+// import "pure-react-carousel/dist/react-carousel.es.css";
 
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  Image,
+  DotGroup,
+  Dot,
+} from "pure-react-carousel";
 export default function Header() {
+  const dotValue = [
+    {
+      key: 1,
+    },
+    {
+      key: 2,
+    },
+    {
+      key: 2,
+    },
+  ];
   return (
-    <header>
+    <header style={{ display: "flex", flexDirection: "column" }}>
       <Navbar />
+      <div id="carousel-container">
+        <CarouselProvider
+          // isPlaying
+          lockOnWindowScroll
+          naturalSlideWidth={400}
+          naturalSlideHeight={800}
+          totalSlides={3}
+          infinite
+          className="carousel-container"
+        >
+          <Slider style={{}} className="carousel-provider">
+            <Slide
+              style={{ height: "auto" }}
+              className="carousel-slide "
+              innerClassName="slideInner___2mfX9"
+              index={0}
+            >
+              {/* <img src={require("../../assets/doctor_patient.png")} /> */}
+              <img
+                style={{
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  width: "100%",
+                  height: "auto",
+                  position: "relative",
+                  borderRadius: 10,
+                  borderWidth: 0,
+                }}
+                className="slider-image"
+                src={require("../../assets/doctor_patient.png")}
+              />
+              {/* <div className="img-overlay"></div> */}
+              <div
+                className="welcome-darex-holder"
+                // style={{
+                //   position: "absolute",
+                //   color: "#00afef",
+                //   marginTop: "25%",
+                //   marginLeft: "25%",
+                //   top: "0%",
+                // }}
+              >
+                <span className="welcome-text">Welcome to</span>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <span className="darex-text">Dare X</span>
+                  <span className="moto-text">One Person One Clinic</span>
+                </div>
+              </div>
+            </Slide>
+            <Slide style={{}} className="carousel-slide" index={0}>
+              {/* <img src={require("../../assets/doctor_patient.png")} /> */}
+              <Image
+                isBgImage
+                style={{
+                  // display: "block",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  width: "100%",
+                  height: "auto",
+                }}
+                className="slider-image"
+                src={require("../../assets/doctor_patient.png")}
+              />
+              <div style={{ position: "absolute" }}>
+                <span>Welcome to</span>
+                <div>
+                  <span>Dare X</span>
+                  <span>One Person One Clinic 2</span>
+                </div>
+              </div>
+            </Slide>
+            <Slide style={{}} className="carousel-slide" index={0}>
+              {/* <img src={require("../../assets/doctor_patient.png")} /> */}
+              <Image
+                isBgImage
+                style={{
+                  // display: "block",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  width: "100%",
+                  height: "auto",
+                }}
+                className="slider-image"
+                src={require("../../assets/doctor_patient.png")}
+              />
+              <div style={{ position: "absolute" }}>
+                <span>Welcome to</span>
+                <div>
+                  <span>Dare X</span>
+                  <span>One Person One Clinic 3</span>
+                </div>
+              </div>
+            </Slide>
+          </Slider>
+          <div className="dot-group">
+            <DotGroup
+              className="dot-group"
+              renderDots={() =>
+                dotValue.map((value, index) => {
+                  return (
+                    // <div style={{ display: "flex", flexDirection: "row" }}>
+                    <Dot className="dot-style" slide={index} />
+                  );
+                })
+              }
+            />
+          </div>
+
+          <div className="button_container">
+            <ButtonBack disabled={false} className="back-button button-style">
+              <ChevronLeft
+                style={{ fontSize: 20 }}
+                color="#005194"
+                className=".MuiIcon-root chevron-style"
+              />
+            </ButtonBack>
+            <ButtonNext disabled={false} className=" next-button button-style">
+              <ChevronRight
+                style={{ fontSize: 20 }}
+                color="#005194"
+                className="chevron-style"
+              />
+            </ButtonNext>
+          </div>
+        </CarouselProvider>
+      </div>
     </header>
   );
 }
--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Grid, Container, Box, Button } from "@material-ui/core";
+import {
+  Paper,
+  Grid,
+  Container,
+  Box,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 // import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
@@ -12,6 +22,13 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary,
+  },
+  menuItem: {
+    "&:hover": {
+      backgroundColor: "#083458",
+      animation: "menu-hover 0.3s ease-in forwards",
+    },
+    color: "#00afef",
   },
 }));
 const navItems = [
@@ -30,35 +47,135 @@ const navItems = [
 ];
 export default function Navbar() {
   const classes = useStyles();
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const [showResizedMenu, setShowResizedMenu] = useState(false);
+  //   const handleClick = () => {
+  //     setShowResizedMenu(!showResizedMenu);
+  //   };
+  const ITEM_HEIGHT = 48;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      console.log("resize");
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    });
+  }, []);
   return (
-    <Grid
-      container
-      spacing={3}
-      alignItems="center"
-      style={{ margin: 0 }}
-      className="main-nav"
-    >
-      <Grid container xs={6}>
-        <Grid item xs={6}>
-          <Box component="div" id="logo-box">
-            <img id="logo" alt="logo" src={require("../../assets/logo.png")} />
-          </Box>
+    <div>
+      <Grid
+        container
+        spacing={0}
+        alignItems="center"
+        style={{ margin: 0 }}
+        className="main-nav"
+      >
+        <Grid
+          justify={windowDimensions.width <= 1090 ? "center" : "flex-start"}
+          alignItems={windowDimensions.width <= 1090 ? "center" : "flex-start"}
+          container
+          xs={6}
+          spacing={0}
+          container
+          xs={6}
+        >
+          <Grid item xs={6}>
+            <Box component="div" id="logo-box">
+              <img
+                id="logo"
+                alt="logo"
+                src={require("../../assets/logo.png")}
+              />
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid justify="center" alignItems="center" container xs={6} spacing={0}>
-        {navItems.map((item, index) => {
-          return (
-            <Grid key={index} item xs={2} justify="center" alignItems="center">
-              <Container className="nav-item">{item.title}</Container>
+        {windowDimensions.width > 1090 && (
+          <Grid
+            justify="center"
+            alignItems="center"
+            container
+            xs={6}
+            spacing={0}
+          >
+            {navItems.map((item, index) => {
+              return (
+                <Grid
+                  key={index}
+                  item
+                  xs={2}
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Container className="nav-item">{item.title}</Container>
+                </Grid>
+              );
+            })}
+            <Grid item xs={2}>
+              <Button variant="outlined" id="sign-up-button">
+                Sign Up
+              </Button>
             </Grid>
-          );
-        })}
-        <Grid item xs={2}>
-          <Button variant="outlined" id="sign-up-button">
-            Sign Up
-          </Button>
-        </Grid>
+          </Grid>
+        )}
+        {windowDimensions.width <= 1090 && (
+          <Grid
+            justify="center"
+            alignItems="center"
+            container
+            xs={6}
+            spacing={0}
+          >
+            <IconButton
+              aria-label="more"
+              aria-controls="long-menu"
+              aria-haspopup="true"
+              onClick={handleClick}
+              style={{ marginLeft: "75%" }}
+            >
+              <MenuIcon style={{ color: "#fff" }} />
+            </IconButton>
+            <Menu
+              id="long-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={open}
+              onClose={handleClose}
+              PaperProps={{
+                style: {
+                  maxHeight: ITEM_HEIGHT * 4.5,
+                  width: "15ch",
+                  marginTop: "40px",
+                },
+              }}
+            >
+              {navItems.map((option) => (
+                <MenuItem
+                  className={classes.menuItem}
+                  key={option}
+                  selected={option === "Pyxis"}
+                  onClick={handleClose}
+                >
+                  {option.title}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Grid>
+        )}
       </Grid>
-    </Grid>
+    </div>
   );
 }
